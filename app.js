8b9db49a1594f36aae987312f595f8dc530cd7e8
Shubham Bartwal <shubhambartwal58@gmail.com>
2022-10-27 23:56:49 +0530

    adding stylesheet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express= require("express");
 const bodyParser= require("body-parser");
 const app = express();
 app.set("view engine","ejs");
+app.use(express.static("public"));
 var listItems =["eat","sleep","repeat"];
 app.use(bodyParser.urlencoded({extended:true})); 
 app.get("/",function(req,res){
