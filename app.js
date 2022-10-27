53187919c5279a8c2b3bd755b68dc8fc49416ca9
Shubham Bartwal <shubhambartwal58@gmail.com>
2022-10-27 23:38:57 +0530

    added new list item by post method

--- app.js
+++ app.js
@@ -2,39 +2,23 @@ const express= require("express");
 const bodyParser= require("body-parser");
 const app = express();
 app.set("view engine","ejs");
+var listItems =["eat","sleep","repeat"];
+app.use(bodyParser.urlencoded({extended:true})); 
 app.get("/",function(req,res){
 var today= new Date();
-var currentDay=today.getDay();
-var day="";
-if(currentDay===0)
-{
-    day="Sunday";
-}
-else if(currentDay===1)
-{
-    day="Monday";
-}
-else if(currentDay===2)
-{
-    day="Tuesday";
-}
-else if(currentDay===3)
-{
-    day="Wednesday";
-}
-else if(currentDay===4)
-{
-    day="Thursday";
-}
-else if(currentDay===5)
-{
-    day="Friday";
-}
-else day="Saturday";
-res.render("list",{kindOfDay: day});  
-
-   // res.send("<h1>Hello</h1>");
+var options={
+    weekday:"long",
+    day:"numeric",
+    month:"long"
+};
+var day=today.toLocaleDateString("en-US",options);
+res.render("list",{kindOfDay: day,listItems:listItems});  
 });
+app.post("/",function(req,res){
+    listItems.push(req.body.newItem);
+    console.log(listItems);
+    res.redirect("/");
+})
 app.listen(3000,function(){
     console.log("Listening to port 3000!!!");
 });
