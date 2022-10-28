60b9513db19dde03a6e5ee518b2fb39c244b3c14
Shubham Bartwal <shubhambartwal58@gmail.com>
2022-10-28 23:46:45 +0530

    added about ,header,footer and workList

--- app.js
+++ app.js
@@ -3,23 +3,36 @@ const bodyParser= require("body-parser");
 const app = express();
 app.set("view engine","ejs");
 app.use(express.static("public"));
-var listItems =["eat","sleep","repeat"];
+let listItems =["eat","sleep","repeat"];
+let workItems=[];
 app.use(bodyParser.urlencoded({extended:true})); 
 app.get("/",function(req,res){
-var today= new Date();
-var options={
+let today= new Date();
+let options={
     weekday:"long",
     day:"numeric",
     month:"long"
 };
-var day=today.toLocaleDateString("en-US",options);
-res.render("list",{kindOfDay: day,listItems:listItems});  
+let day=today.toLocaleDateString("en-US",options);
+res.render("list",{listTitle: day,newlistItems:listItems});  
 });
 app.post("/",function(req,res){
-    listItems.push(req.body.newItem);
-    console.log(listItems);
+  let item=req.body.newItem;
+  if(req.body.list==="Work List"){
+    workItems.push(item);
+res.redirect("/Work");  
+}
+else{
+listItems.push(item);
     res.redirect("/");
-})
+}
+});
+app.get("/Work",function(req,res){
+res.render("list",{listTitle: "Work List",newlistItems:workItems})
+});
+app.get("/About",function(req,res){
+    res.render("About");
+});
 app.listen(3000,function(){
     console.log("Listening to port 3000!!!");
 });
